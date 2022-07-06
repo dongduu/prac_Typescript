e33ca447c1a31a062cbe27ee52e0da335b89af06
dongduu <4012popo@naver.com>
2022-07-06 23:57:11 +0900

    feat: class private

--- index.ts
+++ index.ts
@@ -1,23 +1,7 @@
-type Player<E> = {
-  name: string;
-  extraInfo: E;
-};
-
-type DongduExtra = {
-  favFood: string;
-};
-
-type DonguPlayer = Player<DongduExtra>;
-
-const dongdu: DonguPlayer = {
-  name: "dongdu",
-  extraInfo: {
-    favFood: "banana",
-  },
-};
-
-// name만 가진 프로필 만들기
-const boong: Player<null> = {
-  name: "boong",
-  extraInfo: null,
-};
+class Player {
+  constructor(
+    private firstName: string,
+    private lastName: string,
+    public nickName: string
+  ) {}
+}
