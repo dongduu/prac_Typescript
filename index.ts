cbf8dd7ae970f418fa32444015c02a854566edf1
dongduu <4012popo@naver.com>
2022-07-07 00:31:22 +0900

    feat: abstract class & abstract method

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
-class Player {
+// 추상 클래스 & 추상 메소드
+abstract class User {
   constructor(
-    private firstName: string,
-    private lastName: string,
-    public nickName: string
+    protected firstName: string,
+    protected lastName: string,
+    protected nickName: string
   ) {}
+  abstract getNickName(): void;
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
+
+class Player extends User {
+  getNickName() {
+    console.log(this.nickName);
+  }
+}
+
+const dongdu = new Player("dongjoo", "lee", "동두");
+
+dongdu.getFullName();
