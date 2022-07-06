db94fb9a09b9bf6d99fb6dd29113b37d172f1dea
dongduu <4012popo@naver.com>
2022-07-07 01:02:12 +0900

    feat: hash map

--- index.ts
+++ index.ts
@@ -1,22 +1,34 @@
-// 추상 클래스 & 추상 메소드
-abstract class User {
-  constructor(
-    protected firstName: string,
-    protected lastName: string,
-    protected nickName: string
-  ) {}
-  abstract getNickName(): void;
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
+type Words = {
+  // object 타입 선언 방식
+  [key: string]: string; // "potato" : "food"
+};
+
+class Dict {
+  private words: Words;
+  constructor() {
+    this.words = {};
+  }
+  add(word: Word) {
+    // 클래스를 타입처럼 사용하는 방법
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+  def(term: string) {
+    return this.words[term];
   }
 }
 
-class Player extends User {
-  getNickName() {
-    console.log(this.nickName);
-  }
+class Word {
+  constructor(public term: string, public def: string) {}
 }
 
-const dongdu = new Player("dongjoo", "lee", "동두");
+const kimchi = new Word("kimchi", "한국음식");
 
-dongdu.getFullName();
+const dict = new Dict();
+
+dict.add(kimchi);
+dict.def(kimchi);
+
+console.log(dict);
+console.log(kimchi);
