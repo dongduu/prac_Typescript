a196d1ed8f92b9b62dc2d977afd8597ce5624782
dongduu <4012popo@naver.com>
2022-07-14 18:21:25 +0900

    interface vs type

--- index.ts
+++ index.ts
@@ -1,42 +1,34 @@
-interface User {
-  firstName: string;
+type PlayerA = {
+  name: string;
+};
+
+type PlayerAA = PlayerA & {
   lastName: string;
-  sayHi(name: string): string;
-  fullName(): string;
+};
+
+// type PlayerAA = {
+//    health: number
+//}
+
+interface PlayerB {
+  name: string;
 }
 
-interface Human {
+interface PlayerBB extends PlayerB {
+  lastName: string;
+}
+
+interface PlayerBB {
   health: number;
 }
 
-class Player implements User, Human {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public health: number
-  ) {}
-  fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  sayHi(name: string) {
-    return `Hello ${name}`;
-  }
-}
-
-function makeUser(user: User) {
-  //: User
-  return "hi";
-  //return {
-  // firstName: 'dongduu',
-  // lastName: 'lee',
-  // fullName: () => "xx",
-  // sayHi: (name) => "string"
-  // }
-}
-
-makeUser({
-  firstName: "dongduu",
+const playerA: PlayerAA = {
+  name: "dongdu",
   lastName: "lee",
-  fullName: () => "xx",
-  sayHi: (name) => "string",
-});
+};
+
+const playerB: PlayerBB = {
+  name: "dongdu",
+  lastName: "lee",
+  health: 10,
+};
